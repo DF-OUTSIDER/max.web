69cd6deea35a40b3bc11715c9ef43997b672be57
Atlantis <chinadch@gmail.com>
2017-02-02 10:03:13 +0800

    confirm logout

--- src/main/webapp/resources/js-src/common.ts
+++ src/main/webapp/resources/js-src/common.ts
@@ -1,3 +1,12 @@
 $(function () {
     console.log("Everything Start!");
 });
+
+//global functions
+function confirmLogout() {
+    bootbox.confirm('Are you sure logout?', (result: boolean) => {
+        if (result) {
+            $('#logoutForm').submit();
+        }
+    });
+}
